0c7c2fe72aed14227d8f81b5523fd223dc1e50ea
Yuri <zver22221980@gmail.com>
2022-11-29 14:36:59 +0700

    Дз№ 2 семинар_5.

--- Seminar_5_dz_2/Program.cs
+++ Seminar_5_dz_2/Program.cs
@@ -3,5 +3,24 @@
 // стоящих на нечётных позициях.
 
 // [3, 7, 23, 12] -> 19
-
 // [-4, -6, 89, 6] -> 0
+
+int sum = 0;
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1,10);
+        if (i % 2==0)
+        {
+            sum +=array[i];
+        }
+    }
+    return array;
+}
+int[] array = GetArray(4);
+
+Console.WriteLine(String.Join(", ",array));
+
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " +sum);
