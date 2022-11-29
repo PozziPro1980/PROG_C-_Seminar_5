b0a17f5665bff74e9979120dc02562fc25377e5c
Yuri <zver22221980@gmail.com>
2022-11-29 14:25:56 +0700

    Дз№ 1 семинар_5.

--- Seminar_5_dz_1/Program.cs
+++ Seminar_5_dz_1/Program.cs
@@ -4,16 +4,22 @@
 
 // [345, 897, 568, 234] -> 2
 
+ int count = 0;
 int[] GetArray(int size)
 {
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
         array[i] = new Random().Next(100,1000);
-       
+        if (array[i] % 2==0)
+        {
+            count++;
+        }
     }
     return array;
 }
 int[] array = GetArray(6);
 
-Console.Write(String.Join(", ",array));
+Console.WriteLine(String.Join(", ",array));
+
+Console.WriteLine("Количество чётных чисел в массиве: " +count);
