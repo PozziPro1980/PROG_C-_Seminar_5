a7e2957b65b0b5eb99b865cbb22e3484c210a25c
Yuri <zver22221980@gmail.com>
2022-11-29 13:44:36 +0700

    ДЗ

--- Seminar_5_dz_1/Program.cs
+++ Seminar_5_dz_1/Program.cs
@@ -3,3 +3,17 @@
 // которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+       
+    }
+    return array;
+}
+int[] array = GetArray(6);
+
+Console.Write(String.Join(", ",array));
